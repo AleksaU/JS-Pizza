7f9ab8c916f4c64c7b3c11070ca4e933d1194e39
AleksaU <32110183+AleksaU@users.noreply.github.com>
2017-12-13 13:15:39 +0300

    adding

--- Backend/api.js
+++ Backend/api.js
@@ -9,9 +9,52 @@ exports.getPizzaList = function(req, res) {
 
 exports.createOrder = function(req, res) {
     var order_info = req.body;
-    console.log("Creating Order", order_info);
+
+    var price = 0;
+    var description = "Замовлення піци: " + order_info.name + "\nАдреса доставки: " + order_info.adress + "\nТелефон: "
+        + order_info.phone + "\nЗамовлення:\n";
+    order_info.pizzas.forEach(function (item) {
+        if (item.size == 'big_size') {
+            description += "- " + item.quantity + " шт. [Велика] " + item.pizza.title + ";\n";
+            price += item.quantity * item.pizza.big_size.price;
+        } else if (item.size == 'small_size'){
+            description += "- " + item.quantity + " шт. [Мала] " + item.pizza.title + ";\n";
+            price += item.quantity * item.pizza.small_size.price;
+        }
+    });
+
+    console.log(description);
+
+
+
+    var order = {
+        version: 3,
+        public_key: "i89676865808",
+        action: "pay",
+        amount: price,
+        currency: "UAH",
+        description: description,
+        order_id: Math.random(),
+        sandbox: 1
+    };
+    var data = base64(JSON.stringify(order));
+    var signature = sha1("Lm6y2H4RAsgXUjVrWGx1J0kGe7mmOl42ELNPuqWy" + data + "Lm6y2H4RAsgXUjVrWGx1J0kGe7mmOl42ELNPuqWy");
+
 
     res.send({
-        success: true
+        success: true,
+        data: data,
+        signature: signature
     });
 };
+
+var crypto = require('crypto');
+
+function sha1(string) {
+    var sha1 = crypto.createHash('sha1'); sha1.update(string);
+    return sha1.digest('base64');
+}
+
+function base64(str) {
+    return new Buffer(str).toString('base64');
+}
